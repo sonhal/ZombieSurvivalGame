dc999dc11cb3f3dea2f5fb40915a90c5ce5bf7ce
Sondre <sondre.hal@gmail.com>
2018-02-14 13:44:37 +0100

    added test source folder. added some test tests.

---
+++ tests/engine/AvatarTest.java
@@ -0,0 +1,22 @@
+package engine;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AvatarTest {
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void consumeHealthPack() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pickupWeapon() {
+    }
+
+}
