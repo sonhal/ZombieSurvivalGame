a8872e5d8d02d2f720912ced583e1dfb6f77898c
Snorre Aas <fotballsnorre@hotmail.com>
2018-02-14 16:26:10 +0100

    Oppdatert GUI Menu

--- src/view/Main.java
+++ src/view/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("gamemenu.fxml"));
         primaryStage.setTitle("Zombie Game");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 847, 593));
         primaryStage.show();
     }
 
---
+++ tests/engine/PlayerStatsTest.java
@@ -0,0 +1,17 @@
+package engine;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerStatsTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
