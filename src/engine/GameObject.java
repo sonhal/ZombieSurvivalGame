fc67cf4de0b065dbdf2e1ac353f754153a5cf66c
Julian Sagen <teritry2@gmail.com>
2018-03-17 19:15:26 +0100

    NPC spawn and NPC move towards player

--- src/engine/GameObject.java
+++ src/engine/GameObject.java
@@ -28,8 +28,29 @@ public class GameObject {
     public void move(Direction direction){
         transformComponent.move(direction);
     }
+    public void moveTowards(int playerPosX,int playerPosY){
+        boolean x = this.getTile().cordX > playerPosX;
+        boolean y = this.getTile().cordY > playerPosY;
+        if (x){
+            if (y){
+                if (Math.random() < 0.5) { this.move(Direction.UP);} else{this.move(Direction.RIGHT);}
+            }else {
+                if (Math.random() < 0.5) { this.move(Direction.DOWN);} else{this.move(Direction.RIGHT);}
+            }
+        }else {
+            if (y){
+                if (Math.random() < 0.5) { this.move(Direction.UP);} else{this.move(Direction.LEFT);}
+            }else {
+                if (Math.random() < 0.5) { this.move(Direction.DOWN);} else{this.move(Direction.LEFT);}
+            }
+        }
+
+    }
+
 
     public Sprite getSprite(){
         return graphicsComponent.getSprite();
     }
+
+    public Tile getTile(){ return transformComponent.getCurrentTile();}
 }
